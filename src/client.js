eaa3cd185614603211c639062bf0c483b2e452be
nick <nnutsukpui@gmail.com>
2020-11-10 14:23:36 +0000

    fix for cors issue in cloud

--- src/client.js
+++ src/client.js
@@ -2,6 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://graphql.sketch.cloud/api",
+  fetchOptions: {
+    mode: "no-cors",
+  },
   cache: new InMemoryCache(),
 });
 
