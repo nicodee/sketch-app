e87c6bbb33cf26377aee5b90bc7b00728ca3949d
nick <nnutsukpui@gmail.com>
2020-11-10 16:22:31 +0000

    Removed fetchOptions from apollo client

--- src/client.js
+++ src/client.js
@@ -2,9 +2,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://graphql.sketch.cloud/api",
-  fetchOptions: {
-    mode: "no-cors",
-  },
   cache: new InMemoryCache(),
 });
 
